4db9984a55c3182ad959adb23c3cd56de3d6e691
Brett Schwarz <brett.schwarz@gmail.com>
2018-05-04 08:25:26 -0700

    modified the lat and long, since they were all wrong before

--- src/js/model.js
+++ src/js/model.js
@@ -1,13 +1,13 @@
  var locations = [
-	{title: 'The Nest at the Thompson Hotel', location: {lat: 47.6093511, lng: -122.3422841}, type: 'bar'},
-	{title: 'Seattle Gum Wall', location: {lat: 47.6093511, lng: -122.3422841}, type: 'attraction'},
-	{title: 'Pike Place Market', location: {lat: 47.6093511, lng: -122.3422841}, type: 'attraction'},
-	{title: 'Pike Place Brewery', location: {lat: 47.6093511, lng: -122.3422841}, type: 'bar'},
-	{title: 'Showbox', location: {lat: 47.6093511, lng: -122.3422841}, type: 'entertainment'},
-	{title: 'Hard Rock Cafe', location: {lat: 47.6093511, lng: -122.3422841}, type: 'restaurant'},
-	{title: 'Alibi Room', location: {lat: 47.6093511, lng: -122.3422841}, type: 'restaurant'},
-	{title: 'The Pink Door', location: {lat: 47.6093511, lng: -122.3422841}, type: 'restaurant'},
-	{title: 'Original Starbucks', location: {lat: 47.6093511, lng: -122.3422841}, type: 'coffee'},
-	{title: 'The Athenian Seafood Restaurant and Bar', location: {lat: 47.6090618, lng: -122.3416672}, type: 'restaurant'}
+	{title: 'The Nest at the Thompson Hotel', location: {lat: 47.610722, lng: -122.341404}, type: 'bar'},
+	{title: 'Seattle Gum Wall', location: {lat: 47.608425, lng: -122.340365}, type: 'attraction'},
+	{title: 'Pike Place Market', location: {lat: 47.608599, lng: -122.340704}, type: 'attraction'},
+	{title: 'Pike Place Brewery', location: {lat: 47.608232, lng: -122.339743}, type: 'bar'},
+	{title: 'Showbox', location: {lat: 47.608556, lng: -122.339327}, type: 'entertainment'},
+	{title: 'Hard Rock Cafe', location: {lat: 47.609224, lng: -122.339479}, type: 'restaurant'},
+	{title: 'Alibi Room', location: {lat: 47.608255, lng: -122.340411}, type: 'restaurant'},
+	{title: 'The Pink Door', location: {lat: 47.610365, lng: -122.342560}, type: 'restaurant'},
+	{title: 'Original Starbucks', location: {lat: 47.610090, lng: -122.342470}, type: 'coffee'},
+	{title: 'The Athenian Seafood Restaurant and Bar', location: {lat: 47.608717, lng: -122.341034}, type: 'restaurant'}
 
 ];
