c4f43e4ff103aa2e0fe7c0226b669ab1636ee7c4
Brett Schwarz <brett.schwarz@gmail.com>
2018-05-04 22:20:24 -0700

    re-organizes file, and adds description

--- src/js/model.js
+++ src/js/model.js
@@ -1,13 +1,63 @@
  var locations = [
-	{title: 'The Nest at the Thompson Hotel', location: {lat: 47.610722, lng: -122.341404}, type: 'bar'},
-	{title: 'Seattle Gum Wall', location: {lat: 47.608425, lng: -122.340365}, type: 'attraction'},
-	{title: 'Pike Place Market', location: {lat: 47.608599, lng: -122.340704}, type: 'attraction'},
-	{title: 'Pike Place Brewery', location: {lat: 47.608232, lng: -122.339743}, type: 'bar'},
-	{title: 'Showbox', location: {lat: 47.608556, lng: -122.339327}, type: 'entertainment'},
-	{title: 'Hard Rock Cafe', location: {lat: 47.609224, lng: -122.339479}, type: 'restaurant'},
-	{title: 'Alibi Room', location: {lat: 47.608255, lng: -122.340411}, type: 'restaurant'},
-	{title: 'Pink Door', location: {lat: 47.610365, lng: -122.342560}, type: 'restaurant'},
-	{title: 'Original Starbucks', location: {lat: 47.610090, lng: -122.342470}, type: 'coffee'},
-	{title: 'Athenian Seafood Restaurant and Bar', location: {lat: 47.608717, lng: -122.341034}, type: 'restaurant'}
+	{
+		title: 'The Nest at the Thompson Hotel', 
+		location: {lat: 47.610722, lng: -122.341404}, 
+		type: 'bar',
+		description: 'Great View of the waterfront while enjoying drinks'
+	},
+	{
+		title: 'Seattle Gum Wall',
+		location: {lat: 47.608425, lng: -122.340365},
+		type: 'attraction',
+		description: 'Wall with gum stuck all over it. An unofficial Seattle landmark'
+	},
+	{
+		title: 'Pike Place Market',
+		location: {lat: 47.608599, lng: -122.340704},
+		type: 'attraction',
+		description: 'Market with fresh food and famous for the fish tossing'
+	},
+	{
+		title: 'Pike Brewing Company',
+		location: {lat: 47.608232, lng: -122.339743},
+		type: 'bar',
+		description: 'One of many breweries in Seattle'
+	},
+	{
+		title: 'The Showbox',
+		location: {lat: 47.608556, lng: -122.339327},
+		type: 'entertainment',
+		description: 'Great place to watch live music shows'
+	},
+	{
+		title: 'Hard Rock Cafe Seattle',
+		location: {lat: 47.609224, lng: -122.339479},
+		type: 'restaurant',
+		description: 'Chain restaurant with memorabilia from music industry'
+	},
+	{
+		title: 'Alibi Room',
+		location: {lat: 47.608255, lng: -122.340411},
+		type: 'restaurant',
+		description: 'Restaurant that has movie scripts that can be read. Started by actor Tom Skerritt'
+	},
+	{
+		title: 'The Pink Door',
+		location: {lat: 47.610365, lng: -122.342560},
+		type: 'restaurant',
+		description: 'Trapeze and burlesque shows while eating.'
+	},
+	{
+		title: 'First Starbucks',
+		location: {lat: 47.610090, lng: -122.342470},
+		type: 'coffee',
+		description: 'First Starbucks Coffee shop'
+	},
+	{
+		title: 'Athenian Seafood Restaurant and Bar',
+		location: {lat: 47.608717, lng: -122.341034},
+		type: 'restaurant',
+		description: 'Restaurant made famous by the film Sleepless in Seattle'
+	}
 
 ];
